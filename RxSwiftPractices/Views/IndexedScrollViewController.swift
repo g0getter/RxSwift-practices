193c912a951a56c6858c83590331e88f222beced
Aiden <nagyeong.yeo1023@gmail.com>
2021-09-24 18:20:35 +0900

    IndexedScrollViewController 추가, 화면 이동 연결 전

---
+++ RxSwiftPractices/Views/IndexedScrollViewController.swift
@@ -0,0 +1,19 @@
+//
+//  IndexedScrollViewController.swift
+//  RxSwiftPractices
+//
+//  Created by Aiden on 2021/09/24.
+//
+
+import UIKit
+
+class IndexedScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
