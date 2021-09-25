3f6cbaf28ee14b2828d0d09883bce3f6e3fd4c1c
Aiden <nagyeong.yeo1023@gmail.com>
2021-09-25 21:57:06 +0900

    가로 CollectionView 구현함

---
+++ RxSwiftPractices/Cells/ButtonCell.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonCell.swift
+//  RxSwiftPractices
+//
+//  Created by 여나경 on 2021/09/25.
+//
+
+import UIKit
+
+class ButtonCell: UICollectionViewCell {
+    
+    static let identifier = "MyCell"
+    
+    var button: UIButton?
+    
+    
+}
